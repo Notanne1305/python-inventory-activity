cf6c3a98989bd06e9779e8ddca91fd05a0a66d65
Notanne1305 <j.sindo.545117@umindanao.edu.ph>
2025-10-28 19:11:09 +0800

    hehe

---
+++ dogtranslation.py
@@ -0,0 +1,22 @@
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+    def bark(self):
+        print("Woof! Woof!") 
+        
+    def celebrate_birthday(self):
+        self.age += 1
+        print(f"Happy Birthday! {self.name} is now {self.age} years old.")
+        
+    def show_info(self):
+        print(f"Dog's Name: {self.name}, Age: {self.age}")
+        
+        
+if __name__ == "__main__":
+    my_dog = Dog("Brownie", 5)
+    my_dog.bark()
+    my_dog.celebrate_birthday()
+    my_dog.show_info()
+        
